3ac27d4cde8975d43cecfb1170c1f0bbe49f8325
netannyny <y.yamazaki.sys21@morijyobi.ac.jp>
2022-07-14 14:51:54 +0900

    UserData,DAO 追加

---
+++ servlet01/src/model/UserData.java
@@ -0,0 +1,16 @@
+package model;
+
+public class UserData {
+	private String id;
+	private String pass;
+	private String name;
+
+	public UserData() {}
+	public UserData(String _id,String _pass,String _name) {
+		id = _id; pass = _pass; name = _name;
+	}
+	public String getId() {return id;}
+	public String getPass() {return pass;}
+	public String getName() {return name;}
+
+}
